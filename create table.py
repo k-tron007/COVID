3b2b0eea554bfdc6e8f8807b611bb1db703c048a
K-tron <k.imayavaramban@outlook.com>
2022-02-21 10:01:30 +0530

    update

--- create table.py
+++ create table.py
@@ -2,4 +2,6 @@ import mysql.connector
 conn=mysql.connector.connect(host='localhost', user='root', password='miok0909', db='archit')
 cursor=conn.cursor()
 cursor.execute("create table COVID (Date date, Active int, Confirmed int, Recovered int, Death int)")
+cursor.execute("create table deleted_data (Date date, Active int, Confirmed int, Recovered int, Death int, Deleted_on date)")
+cursor.execute("create table altered_data (Date date, iAct int, fAct int, iCon int, fCon int, iRec int ,fRec int, iDet int, fDet int, Altered_on date)")
 conn.commit()
